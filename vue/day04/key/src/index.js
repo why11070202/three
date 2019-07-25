08513b25050850ae9ffe3057d5a9aeedbbee6b9a
why <1005580669@qq.com>
2019-07-25 22:05:41 +0800

    day04-2

--- vue/day04/key/src/index.js
+++ vue/day04/key/src/index.js
@@ -4,18 +4,26 @@ new Vue({
     el: '#box',
     data: {
         html: "你是谁哈啊山东省青岛市广东省victoria",
+        html2: "你是谁哈啊山东省青岛市广东省victoria",
         key: null,
     },
     template: `<div >
-                    <input v-model="key"/>
+                    <input v-model="key" />
+                    <p v-html="html" :style="{fontSize:'22px'}"></p>
                     <p v-html="newhtml" :style="{fontSize:'22px'}"></p>
                 </div>
                 `,
+    watch: {
+        key: function () {
+            const reg = new RegExp(this.key, 'gi');
+            this.html = this.html2.replace(reg, `<span style="color:red;">${this.key}</span>`)
+        }
+    },
 
     computed: {
         newhtml() {
             const reg = new RegExp(this.key, 'gi');
-            return this.html.replace(reg, '<span style="color:red";}>' + this.key + '</span>')
+            return this.html2.replace(reg, '<span style="color:yellow";}>' + this.key + '</span>')
             // vm.html = "<b>hah</b>"
         }
     }
