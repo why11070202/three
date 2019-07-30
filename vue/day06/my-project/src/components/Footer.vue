a6fe44f5846522aa4572d1ad2a99effa513d2b0c
why <1005580669@qq.com>
2019-07-30 08:43:26 +0800

    day06

---
+++ vue/day06/my-project/src/components/Footer.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div id="vUVGmfjZxjJGKJAPcW" class="fPCjysaTa sdk-top-banner">
+      <div class="UHHyhEjrWPZnPnhmxqyhgaI Ropeylr GuiwlGNbi" style="display: block !important;">
+        <a href="//d.toutiao.com/N13p/" target="_self" class="download" rel="nofollow">
+          <div class="logo-wrapper">
+            <div class="logo"></div>
+            <div class="banner-label">
+              <p class="tb" data-node="appName">今日头条</p>
+              <p class="title-sub"></p>
+            </div>
+          </div>
+          <div class="open">打开</div>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+#vUVGmfjZxjJGKJAPcW {
+  position: fixed;
+  height: 50px;
+  bottom: 0;
+  background: #fff;
+}
+#vUVGmfjZxjJGKJAPcW {
+  height: 50px;
+  box-sizing: content-box;
+  font-family: STHeiti, Microsoft YaHei, Helvetica, Arial, sans-serif;
+  word-break: break-word;
+  width: 100%;
+  z-index: 5000;
+}
+
+#vUVGmfjZxjJGKJAPcW .download {
+  height: 50px;
+  padding: 0 30px;
+  display: flex;
+  justify-content: space-between;
+  box-sizing: border-box;
+  align-items: center;
+  width: 100%;
+  text-decoration: none;
+}
+#vUVGmfjZxjJGKJAPcW .logo-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+#vUVGmfjZxjJGKJAPcW .logo {
+  height: 36px;
+  width: 36px;
+  background: url(//s3.pstatp.com/growth/fe_sdk/image/banner_toutiao_icon_bce0b302.png)
+    no-repeat;
+  overflow: hidden;
+  background-size: 100% 100%;
+  border: 1px solid #eee;
+  border-radius: 7px;
+  box-sizing: border-box;
+}
+
+#vUVGmfjZxjJGKJAPcW .open {
+  width: 72px;
+  height: 28px;
+  font-size: 14px;
+  line-height: 28px;
+  border-radius: 14px;
+  font-weight: 500;
+  background-image: linear-gradient(90deg, red, #ff5050);
+  color: #fff;
+  text-align: center;
+}
+</style>
---
+++ vue/day06/my-project/src/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <header id="indexHeader" class="index--toutiaoribao">
+      <div class="top_bar">
+        <div class="abs_m">
+          <a href="#" class="refresh_title btn"></a>
+          <div class="refreshBtn-container">
+            <i :class="{refresh_btn:1, rotate:bool2}" @click="change"></i>
+          </div>
+        </div>
+        <div class="abs_l">
+          <a href="javascript:;" class="msg-box" @click="toggle"></a>
+        </div>
+        <div class="abs_r">
+          <a href="###" class="btn search"></a>
+        </div>
+      </div>
+    </header>
+
+    <div>
+      <div data-reactroot id="pageletPopupBanner" class="tt-modal" v-show="bool">
+        <div class="popup">
+          <div class="banner"></div>
+          <p>已加载好您感兴趣的头条</p>
+          <div class="download-btn" data-node="downloadBtn">立即打开</div>
+          <div class="close" data-node="close" @click="toggle"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      bool: false,
+      bool2: false,
+      timer: null
+    };
+  },
+  methods: {
+    toggle() {
+      this.bool = !this.bool;
+    },
+    change() {
+      this.bool2 = !this.bool2;
+      this.timer = setTimeout(function() {
+        this.bool2 = !this.bool2;
+      }, 1000);
+    }
+  }
+};
+</script>
+
